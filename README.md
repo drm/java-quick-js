1fa098101dc57e716df9e3180fe90c40d2ea8863
Gerard van Helden <gerard@van-helden.name>
2023-04-20 13:12:51 +0100

    add failing pool test (memory error) + more docs

--- README.md
+++ README.md
@@ -3,34 +3,43 @@
 This provides a Java Native Interface (JNI) to
 [QuickJS](https://bellard.org/quickjs/).
 
+It's stable and I see no reason not to start using it in production if you see
+
+
 ## API compatibility
 
-This implementation currently only supports the 'lambda'-approach of javascripts,
-i.e., useful for server-side rendering but not for server-side logic. Accessing
-files, server resources and other I/O are not tested nor supported, for now.
+This implementation currently only supports the 'lambda'-approach of
+javascripts, i.e., useful for server-side rendering but not for server-side
+logic. Accessing files, server resources and other I/O are not tested nor
+supported, for now.
 
-It is probably not hard to implement, so let me know if you're interested in 
+It is probably not hard to implement, so let me know if you're interested in
 either contributing or persuading me to implement it.
 
 ## Preliminary testing results
 
-I started this project to research performance in comparison with V8. Obviously, 
-V8 is much more sophisticated and optimized, so for common workloads, V8 is 
-probably the better choice.
+I started this project to research performance in comparison with V8.
+Obviously, V8 is much more sophisticated and optimized, so for common
+workloads, V8 is probably the better choice.
 
-However, this library seems to outperform V8 by an order of magnitude, for
-*small* one-off calls and low memory consumption.
+However, quickjs seems to outperform V8 by an order of magnitude, for *small*
+one-off calls and low memory consumption. Also, the garbage collector is
+deterministic and it's therefore much easier to hold the reign of this puppy
+than to the monstrous power of V8.
 
-I haven't really devised any definitive performance test, but based on some 
-real-world testing, V8 starts to outshine at higher memory consumption and 
-JIT optimisation by a landslide.
+I haven't really devised any definitive performance test, but based on some
+real-world testing, V8 starts to outshine at higher memory consumption and JIT
+optimisation by a landslide. Yes, dear frontenders, a 5MB bundle file of JS
+code may be a bit much... Maybe the backend really isn't the problem here.
 
-Nonetheless, if you don't expect high loads, and your here for simplicity rather
-than performance, QuickJS is much easier to build (takes about 25 seconds on my
-machine, rather than the 20+ minutes (!) for V8...).
+Nonetheless, if you don't expect high loads, and your here for simplicity
+rather than performance, QuickJS is much easier to build (takes about 25
+seconds on my machine, rather than the 20+ minutes (!) for V8...).
 
 Take a look at [mv8](https://github.com/drm/mv8) if you need V8 bindings in
-Java.
+Java. It's being used successfully in multiple high-traffic websites
+notwithstanding the lack of documentation. Feel free to drop me a note if you
+need help.
 
 ## Usage
 
@@ -49,9 +58,9 @@ try (var rt = Qjs.createRuntime()) {
 
 ### Lightweight and efficient context boot-up
 
-The design of qjs is such that you can cheaply create contexts and duplicate them, 
-so you can have a blueprint of the context, preloaded with you libraries, and
-duplicate that template to actually execute your code in.
+The design of qjs is such that you can cheaply create contexts and duplicate
+them, so you can have a blueprint of the context, preloaded with you libraries,
+and duplicate that template to actually execute your code in.
 
 To speed up loading the libraries, you can precompile them to bytecode.
 
@@ -81,6 +90,10 @@ try (var rt = Qjs.createRuntime()) {
     }
 } 
 ```
+
+### More scenario's
+
+Read the tests for more implementation examples.
 
 # Questions, comments or remarks?
 
--- test/java/nl/melp/qjs/QuickjsTest.java
+++ test/java/nl/melp/qjs/QuickjsTest.java
@@ -11,6 +11,13 @@ import nl.melp.qjs.Qjs.Context;
 import nl.melp.qjs.Qjs.Runtime;
 
 import java.nio.file.Path;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 import static nl.melp.qjs.Assert.assertEquals;
 
@@ -131,30 +138,186 @@ public class QuickjsTest {
 		assertEquals(true, Qjs.compile(srcFile, binary));
 		final long compilation_dur_ns = (System.nanoTime() - t_start);
 		long total_duplication_ns = 0;
-		try (Runtime rt = Qjs.createRuntime()) {
-			try (Context template = rt.createContext()) {
-				template.evalBinaryPath(binary);
+		long total_binary_eval_ns = 0;
 
-				for (int i = 0; i < num_runs; i ++) {
-					long t_pre = System.nanoTime();
-					Context c = template.duplicate();
-					total_duplication_ns += (System.nanoTime() - t_pre);
-					c.evalPath(Path.of("resources/js/realworld/input.js"));
-					c.close();
-				}
+		Runtime rt = null;
+		Context template = null;
+		int i = 0;
+		do {
+			if (rt == null) {
+				rt = Qjs.createRuntime();
+				template = rt.createContext();
+				long t_pre = System.nanoTime();
+				template.evalBinaryPath(binary);
+				total_binary_eval_ns += System.nanoTime() - t_pre;
 			}
+			long t_pre = System.nanoTime();
+			Context c = template.duplicate();
+			total_duplication_ns += (System.nanoTime() - t_pre);
+			c.evalPath(Path.of("resources/js/realworld/input.js"));
+			c.close();
+			if (i % 20 == 19) {
+				template.close();
+				rt.close();
+				rt = null;
+				template = null;
+			}
+		} while (i ++ < num_runs);
+		if (rt != null) {
+			template.close();
+			rt.close();
 		}
+
 		final long dur_ns = (System.nanoTime() - t_start);
 		System.out.println(total_duplication_ns);
 		System.out.printf(
-			"Num runs: %d, total runtime: %.2f s, compilation time: %.2f ms, duplication time: %.2f s, avg(incl. compilation): %.2f ms/c, avg(without compilation): %.2f ms/c\n",
+			"Num runs: %d, total runtime: %.2f s, compilation time: %.2f ms, duplication time: %.2f s, total_binary_eval_ns: %.2f s, avg(incl. compilation): %.2f ms/c, avg(without compilation): %.2f ms/c\n",
 			num_runs,
 			dur_ns / 1_000_000_000F,
 			compilation_dur_ns / 1_000_000F,
 			total_duplication_ns / 1_000_000_000F,
+			total_binary_eval_ns / 1_000_000_000F,
 			dur_ns / 1_000_000F / num_runs,
 			(dur_ns - compilation_dur_ns) / 1_000_000F / num_runs
 		);
+	}
+
+	public void testPrecompiledPoolPerformance() throws Exception {
+		final Path srcFile = Path.of("resources/js/realworld/combined.js");
+		if (!srcFile.toFile().exists()) {
+			// this is an actual real world example that is not part of the public
+			// repository.
+			return;
+		}
+
+		class Pool<T extends AutoCloseable> {
+			private final BlockingQueue<T> pool;
+			private final ExecutorService producer;
+
+			private boolean stop = false;
+
+			Pool(Supplier<T> supplier, int size) {
+				this.pool = new ArrayBlockingQueue<T>(size);
+				this.producer = Executors.newSingleThreadExecutor();
+
+				this.producer.submit(() -> {
+					T item = supplier.get();
+					do {
+						try {
+							if (pool.offer(item, 500, TimeUnit.MILLISECONDS)) {
+								System.out.printf("offered and accepted at %d%n", pool.size());
+								item = supplier.get();
+							} else {
+								System.out.printf("offered, but failed at %d, [stop=%b]%n", pool.size(), stop);
+							}
+						} catch (InterruptedException e) {
+							stop = true;
+						}
+					} while (!stop);
+
+					if (item != null) {
+						try {
+							item.close();
+						} catch (Exception e) {
+							e.printStackTrace();
+							// continue anyway.
+						}
+					}
+
+					do {
+						item = pool.poll();
+						if (item != null) {
+							try {
+								item.close();
+							} catch (Exception e) {
+								e.printStackTrace();
+								// continue anyway.
+							}
+						}
+					} while (item != null);
+				});
+			}
+
+			<R> R with(Function<T, R> t) throws InterruptedException {
+				try (T c = take()) {
+					return t.apply(c);
+				} catch (InterruptedException e) {
+					Thread.currentThread().interrupt();
+					throw new InterruptedException();
+				} catch (Exception e) {
+					throw new RuntimeException(e);
+				}
+			}
+
+			private T take() throws InterruptedException {
+				try {
+					return pool.take();
+				} catch (InterruptedException e) {
+					Thread.currentThread().interrupt();
+					throw new InterruptedException();
+				}
+			}
+
+			void close() throws Exception {
+				this.stop = true;
+				this.producer.shutdown();
+				final boolean b = this.producer.awaitTermination(1, TimeUnit.SECONDS);
+				if (!b) {
+					throw new RuntimeException("Could not stop executor");
+				}
+				AutoCloseable c;
+				while (null != (c = pool.poll())) {
+					c.close();
+				}
+			}
+		}
+
+		class ContextPool implements AutoCloseable {
+			private final Runtime runtime;
+			private final Context template;
+			private final Pool<Context> pool;
+
+			public ContextPool() {
+				this.runtime = Qjs.createRuntime();
+				this.template = this.runtime.createContext();
+				this.pool = new Pool<>(this::duplicateContext, 40);
+				final Path binary = Path.of("resources/js/realworld/combined.js.bin");
+				this.template.evalBinaryPath(binary);
+			}
+
+			private Context duplicateContext() {
+				return this.template.duplicate();
+			}
+
+			@Override
+			public void close() throws Exception {
+				this.pool.close();
+				this.template.close();
+				this.runtime.close();
+			}
+
+			<R> R with(Function<Context, R> f) throws InterruptedException {
+				return pool.with(f);
+			}
+		}
+
+		try (var pool = new ContextPool()) {
+			for (int i = 0; i < num_runs; i ++) {
+				pool.with((c) -> c.evalPath(Path.of("resources/js/realworld/input.js")));
+			}
+		}
+
+//		System.out.println(total_duplication_ns);
+//		System.out.printf(
+//			"Num runs: %d, total runtime: %.2f s, compilation time: %.2f ms, duplication time: %.2f s, total_binary_eval_ns: %.2f s, avg(incl. compilation): %.2f ms/c, avg(without compilation): %.2f ms/c\n",
+//			num_runs,
+//			dur_ns / 1_000_000_000F,
+//			compilation_dur_ns / 1_000_000F,
+//			total_duplication_ns / 1_000_000_000F,
+//			total_binary_eval_ns / 1_000_000_000F,
+//			dur_ns / 1_000_000F / num_runs,
+//			(dur_ns - compilation_dur_ns) / 1_000_000F / num_runs
+//		);
 	}
 
 	public void testPrecompiledTemplatePerformance() throws Exception {
